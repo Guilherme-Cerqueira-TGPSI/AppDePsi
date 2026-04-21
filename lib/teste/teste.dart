19ccca6b70037c281b97fd600d4d8ce889cd2a45
Guilherme <studio827834@gmail.com>
2026-04-21 11:34:33 +0100

    teste

---
+++ lib/teste/teste.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class teste extends StatelessWidget{
+  const teste ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
